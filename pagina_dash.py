7e0a6c8f2f5e6ad1bb0dbbcce5c9adff2df1dc8b
Ardark27 <pacualadri@gmail.com>
2022-06-15 17:10:05 +0200

    features added : data type selection, markers to the graph

--- pagina_dash.py
+++ pagina_dash.py
@@ -29,6 +29,13 @@ app.layout = html.Div([
             html.H6("Date: "),
             dcc.Dropdown(
                 id='option-date'
+            ),
+            html.H6("Data Avaliable"),
+            dcc.RadioItems(
+                id='option-data-available',
+                options=[{'label': i, 'value': i} for i in ['prices', 'impliedVolatility']],
+                value='impliedVolatility',
+                inline= True
             )
         ],
         style={'width': '48%', 'display': 'inline-block'}),
@@ -58,14 +65,15 @@ def set_cities_value(option_date):
 @app.callback(
     Output('display-selected-values', 'figure'),
     Input('option-date', 'value'),
-    Input('option-type', 'value'))
-def set_display_children(option_date, option_type):
+    Input('option-type', 'value'),
+    Input('option-data-available', 'value'))
+def set_display_children(option_date, option_type, option_data_availiable):
     for i in a.keys():
         strike = a[i][option_type][option_date]['strikes']
-        volatility = a[i][option_type][option_date]['impliedVolatility']
+        data = a[i][option_type][option_date][option_data_availiable]
     
-    df = pd.DataFrame({'Strike': strike, 'Implied Volatility': volatility})
-    fig = px.line(df, x='Strike', y='Implied Volatility')
+    df = pd.DataFrame({'Strike': strike, option_data_availiable : data})
+    fig = px.line(df, x='Strike', y=f'{option_data_availiable}', markers = True)
     return fig
 
 if __name__ == '__main__':
