198ef495ef00371c02d07e68c37fed66dc216ce0
chalangui <chalanguiasm@gmail.com>
2022-06-21 20:50:23 +0200

    add .env

--- src/db.py
+++ src/db.py
@@ -1,22 +1,21 @@
-from textwrap import indent
 import boto3
-from boto3.dynamodb.conditions import Key
 import os
 import pandas as pd
 import json
 
-# ID_KEY = os.environ.get('ID_KEY', None)
-# SECRET_KEY = os.environ.get('AWS_SECRET_KEY', None)
-# DB_TABLE = os.environ.get('DB_TABLE', None)
+ID_KEY = os.environ.get('ID_KEY', None)
+SECRET_KEY = os.environ.get('AWS_SECRET_KEY', None)
+DB_TABLE = os.environ.get('DB_TABLE', None)
+REGION = os.environ.get('REGION', None)
 
 session = boto3.Session(
-    aws_access_key_id='AKIA24TIIEHEDZUTAOE7',
-    aws_secret_access_key='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',
-    region_name='eu-west-3'
+    aws_access_key_id=ID_KEY,
+    aws_secret_access_key=SECRET_KEY,
+    region_name=REGION
 )
 dynamodb = session.resource('dynamodb')
 
-table = dynamodb.Table('option_data_ibex')
+table = dynamodb.Table(DB_TABLE)
 
 def get_data_from_date(date):
     response = table.get_item(
@@ -30,5 +29,3 @@ def get_all_dates():
     response = table.scan(AttributesToGet=['date'])
     data = pd.DataFrame.from_dict(response['Items']).sort_values('date').date.values.tolist()
     return data
-
-print(get_all_dates())
